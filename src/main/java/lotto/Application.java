027e67c64c24c958a58cb8d81c65bb592ae6d4c2
sejin <psj8880@naver.com>
2025-10-29 00:27:05 +0900

    feat(Application): 조립 및 프로그램 실행 구현
    - 구현체를 생성하고, 프로그램을 실행하도록 구현
    - try - catch 를 통해 예외 처리
    - 예외가 발생하면 다시 실행하도록 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.domain.port.PickRandomNumbers;
+import lotto.infra.random.DefaultPickRandomValues;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        PickRandomNumbers pickRandomNumbers = new DefaultPickRandomValues();
+        OutputView outputView = new OutputView();
+
+        Controller controller = new Controller(inputView, pickRandomNumbers, outputView);
+
+        while (true) {
+            try {
+                controller.run();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 }
