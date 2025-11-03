144e98214bea308e78bbf112f3828a60a1a2c30f
sejin <psj8880@naver.com>
2025-11-03 15:57:10 +0900

    feat(Controller): (입력 - LottoUsecase 전달 - 출력) 로직 구현
    - 입력이 올바른 경우에만 해당 도메인을 생성하고, 그렇지 않은 경우에는 다시 입력 받도록 하면서 input과 도메인이 한 로직에 묶여 있어, 도메인 생성 부분을 UseCase가 아닌 Controller가 담당하도록 하였음
    - 입력을 통해 도메인을 생성하고, 도메인을 UseCase에 넘겨서 서비스 로직을 수행해 얻은 결과를 Controller에서 출력하도록 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -5,48 +5,73 @@ import static lotto.view.InputParser.*;
 import java.util.List;
 import lotto.domain.BonusNumber;
 import lotto.domain.LottoCart;
-import lotto.domain.LottoGenerator;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningLotto;
 import lotto.domain.WinningNumbers;
-import lotto.domain.port.PickRandomNumbers;
-import lotto.service.LottoGame;
 import lotto.service.ResultSheet;
+import lotto.view.InputRetryHandler;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Controller {
 
     private final InputView inputView;
-    private final PickRandomNumbers pickRandomNumbers;
+    private final LottoUseCase lottoUseCase;
     private final OutputView outputView;
 
-    public Controller(InputView inputView, PickRandomNumbers pickRandomNumbers, OutputView outputView) {
+    public Controller(InputView inputView, LottoUseCase lottoUseCase, OutputView outputView) {
         this.inputView = inputView;
-        this.pickRandomNumbers = pickRandomNumbers;
+        this.lottoUseCase = lottoUseCase;
         this.outputView = outputView;
     }
 
     public void run() {
-        String amountInput = validateBlank(inputView.purChaseAmountReadLine());
-        PurchaseAmount purchaseAmount = new PurchaseAmount(amountInput);
-        LottoGenerator lottoGenerator = new LottoGenerator(pickRandomNumbers);
+        PurchaseAmount purchaseAmount = getPurchaseAmount();
 
-        LottoCart lottoCart = lottoGenerator.generateLottoList(purchaseAmount);
+        LottoCart lottoCart = lottoUseCase.generateLottoCart(purchaseAmount);
         outputView.printLottoCount(lottoCart);
         outputView.printLottoCart(lottoCart);
 
-        List<String> winningNumbersInput = parseValidateNumbers(inputView.winningNumbersReadLine());
-        WinningNumbers winningNumbers = new WinningNumbers(winningNumbersInput);
+        WinningNumbers winningNumbers = getWinningNumbers();
+        BonusNumber bonusNumber = getBonusNumber();
+        WinningLotto winningLotto = validWinningLotto(winningNumbers, bonusNumber);
 
-        String bonusInput = validateBlank(inputView.bonusNumberReadLine());
-        BonusNumber bonusNumber = new BonusNumber(bonusInput, winningNumbers);
+        ResultSheet resultSheet = lottoUseCase.compare(winningLotto, lottoCart);
+        double rateOfReturn = lottoUseCase.calculateRateOfReturn(winningLotto, lottoCart, purchaseAmount);
 
-        LottoGame lottoGame = new LottoGame();
-        ResultSheet resultSheet = lottoGame.game(lottoCart, winningNumbers, bonusNumber);
-        double rateOfReturn = lottoGame.calculateRateOfReturn(resultSheet,purchaseAmount);
         outputView.printWinningStatistics(resultSheet);
         outputView.printRateOfReturn(rateOfReturn);
+    }
 
+    private BonusNumber getBonusNumber() {
+        return InputRetryHandler.askUtilValid(() -> {
+            String bonusInput = validateBlank(inputView.bonusNumberReadLine());
+            return new BonusNumber(changeType(bonusInput));
+        });
+    }
 
+    private WinningNumbers getWinningNumbers() {
+        return InputRetryHandler.askUtilValid(() -> {
+            List<String> winningNumbersInput = parseValidateNumbers(inputView.winningNumbersReadLine());
+            return new WinningNumbers(changeType(winningNumbersInput));
+        });
+    }
+
+    private PurchaseAmount getPurchaseAmount() {
+        return InputRetryHandler.askUtilValid(() -> {
+            String amountInput = validateBlank(inputView.purChaseAmountReadLine());
+            return new PurchaseAmount(changeType(amountInput));
+        });
+    }
+
+    private WinningLotto validWinningLotto(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        while (true) {
+            try {
+                return new WinningLotto(winningNumbers, bonusNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                bonusNumber = getBonusNumber();
+            }
+        }
     }
 }
