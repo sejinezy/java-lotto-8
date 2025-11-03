d749506a23d76ca5584597ec3f4ad738c056ad4a
sejin <psj8880@naver.com>
2025-11-03 15:59:08 +0900

    refactor(Application): 조립과 controller 실행 구현 / 예외처리 로직 삭제
    - 기존 코드는 예외 발생시 처음부터 로직이 다시 수행되었음
    - 예외가 발생한 지점부터 다시 입력 받도록 하기 위해 코드 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,25 +2,21 @@ package lotto;
 
 import lotto.domain.port.PickRandomNumbers;
 import lotto.infra.random.DefaultPickRandomValues;
+import lotto.service.LottoGame;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        PickRandomNumbers pickRandomNumbers = new DefaultPickRandomValues();
         OutputView outputView = new OutputView();
+        PickRandomNumbers pickRandomNumbers = new DefaultPickRandomValues();
+        LottoGame lottoGame = new LottoGame();
 
-        Controller controller = new Controller(inputView, pickRandomNumbers, outputView);
+        LottoUseCase lottoUseCase = new LottoUseCase(pickRandomNumbers, lottoGame);
 
-        while (true) {
-            try {
-                controller.run();
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        Controller controller = new Controller(inputView, lottoUseCase, outputView);
+        controller.run();
 
     }
 }
