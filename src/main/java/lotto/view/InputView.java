ba66f31537d744b31d3d4c7dc62bb133bb501780
sejin <psj8880@naver.com>
2025-11-03 14:35:29 +0900

    refactor(InputView): printf 구문을 사용하도록 수정
    - "줄 바꿈"여부가 상수 메시지에 한 눈에 드러날 수 있도록 printf와 %n을 사용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,23 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.%n";
+    private static final String INPUT_WINNING_NUMBERS = "%n당첨 번호를 입력해 주세요.%n";
+    private static final String INPUT_BONUS_NUMBER = "%n보너스 번호를 입력해 주세요.%n";
 
     public String purChaseAmountReadLine() {
-        System.out.println(INPUT_PURCHASE_AMOUNT);
+        System.out.printf(INPUT_PURCHASE_AMOUNT);
         return Console.readLine();
     }
 
     public String winningNumbersReadLine() {
-        System.out.println(INPUT_WINNING_NUMBERS);
+        System.out.printf(INPUT_WINNING_NUMBERS);
         return Console.readLine();
     }
 
     public String bonusNumberReadLine() {
-        System.out.println();
-        System.out.println(INPUT_BONUS_NUMBER);
+        System.out.printf(INPUT_BONUS_NUMBER);
         return Console.readLine();
     }
 }
