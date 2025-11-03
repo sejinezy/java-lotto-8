68023ddea20f55b2881aa5803611da9dbfdbce03
sejin <psj8880@naver.com>
2025-11-03 14:29:32 +0900

    feat(Rank): enum Rank의 필드 추가, 메서드 추가 구현
    - 보너스 번호 당첨 여부도 Rank를 결정하는 요소 중 하나이므로 enum Rank의 필드에 추가
    - 당첨 번호 개수와 보너스 번호 당첨 여부를 확인해 해당 Rank를 반환하는 메서드 구현

--- src/main/java/lotto/service/Rank.java
+++ src/main/java/lotto/service/Rank.java
@@ -1,15 +1,26 @@
 package lotto.service;
 
 public enum Rank {
-    FIRST(6,2000000000),SECOND(5,30000000),
-    THIRD(5,1500000), FOURTH(4,50000), FIFTH(3, 5000);
+    FIRST(6, false, 2000000000), SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000), FOURTH(4, false, 50000), FIFTH(3, false, 5000);
 
     private final int numberOfMatches;
+    private final boolean hasBonus;
     private final int price;
 
-    Rank(int numberOfMatches, int price) {
+    Rank(int numberOfMatches, boolean hasBonus, int price) {
         this.numberOfMatches = numberOfMatches;
+        this.hasBonus = hasBonus;
         this.price = price;
+    }
+
+    public static Rank findByNumberOfMatchesAndHasBonus(int numberOfMatches, boolean hasBonus) {
+        for (Rank rank : values()) {
+            if (rank.getNumberOfMatches() == numberOfMatches && rank.hasBonus == hasBonus) {
+                return rank;
+            }
+        }
+        return null;
     }
 
     public int getNumberOfMatches() {
