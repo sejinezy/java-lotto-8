8f5c9234bf1affa999272c7e5d51fc565956fd8f
sejin <psj8880@naver.com>
2025-11-03 15:53:28 +0900

    feat(LottoUseCase): 프로그램 서비스 흐름 구현
    - 구매 금액만큼 로또 생성해서 로또 카트 반환
    - 수익률 계산
    - 결과지 작성

---
+++ src/main/java/lotto/LottoUseCase.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import lotto.domain.LottoCart;
+import lotto.domain.LottoGenerator;
+import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningLotto;
+import lotto.domain.port.PickRandomNumbers;
+import lotto.service.LottoGame;
+import lotto.service.ResultSheet;
+
+public class LottoUseCase {
+
+    private final PickRandomNumbers pickRandomNumbers;
+    private final LottoGame lottoGame;
+
+    public LottoUseCase(PickRandomNumbers pickRandomNumbers, LottoGame lottoGame) {
+        this.pickRandomNumbers = pickRandomNumbers;
+        this.lottoGame = lottoGame;
+    }
+
+    public LottoCart generateLottoCart(PurchaseAmount purchaseAmount) {
+        LottoGenerator lottoGenerator = new LottoGenerator(pickRandomNumbers);
+        return lottoGenerator.generateLottoCart(purchaseAmount);
+    }
+
+    public double calculateRateOfReturn(WinningLotto winningLotto, LottoCart lottoCart, PurchaseAmount purchaseAmount) {
+        ResultSheet resultSheet = compare(winningLotto, lottoCart);
+        return lottoGame.calculateRateOfReturn(purchaseAmount, resultSheet);
+    }
+
+    public ResultSheet compare(WinningLotto winningLotto, LottoCart lottoCart) {
+        return lottoGame.compare(winningLotto, lottoCart);
+    }
+
+}
