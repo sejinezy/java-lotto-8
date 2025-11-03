96a76d4e3ddd1c67a3b80230946b9bf66c76e6f3
sejin <psj8880@naver.com>
2025-11-03 22:41:22 +0900

    feat(OutputView): 추가 메서드 구현 및 리팩터링
    - 에러메시지를 출력하는 기능 구현
    - static 클래스로 리팩터링
    - 맵 대신 Rank.values()를 사용해 출력 순서 고정
    - Rank.SECOND 매직 넘버 노출을 막기 위해 hasBonus() 대신 사용

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,7 +3,6 @@ package lotto.view;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map.Entry;
 import lotto.domain.Lotto;
 import lotto.domain.LottoCart;
 import lotto.domain.Rank;
@@ -16,12 +15,16 @@ public class OutputView {
     public static final String RESULT_SHEET_HAS_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개%n";
     public static final String RESULT_SHEET_DONT_HAVE_MESSAGE = "%d개 일치 (%,d원) - %d개%n";
     public static final String PRINT_RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.%n";
+    public static final String PREFIX_ERROR_MESSAGE = "[ERROR] ";
 
-    public void printLottoCount(LottoCart lottoCart) {
+    private OutputView() {
+    }
+
+    public static void printLottoCount(LottoCart lottoCart) {
         System.out.printf(PRINT_LOTTO_CART_MESSAGE, lottoCart.lottoCart().size());
     }
 
-    public void printLottoCart(LottoCart lottoCart) {
+    public static void printLottoCart(LottoCart lottoCart) {
         for (Lotto lotto : lottoCart.lottoCart()) {
             List<Integer> numbers = new ArrayList<>(lotto.numbers());
             numbers.sort(Comparator.naturalOrder());
@@ -29,26 +32,28 @@ public class OutputView {
         }
     }
 
-    public void printWinningStatistics(ResultSheet resultSheet) {
+    public static void printWinningStatistics(ResultSheet resultSheet) {
         System.out.println();
         System.out.printf(WINNING_STATISTICS_MESSAGE);
 
-        for (Entry<Rank, Integer> entry : resultSheet.getResultSheet().entrySet()) {
-            Rank rank = entry.getKey();
-            Integer count = entry.getValue();
-
+        for (Rank rank : Rank.values()) {
+            int count = resultSheet.getCount(rank);
             System.out.printf(getMessageBy(rank), rank.getNumberOfMatches(), rank.getPrice(), count);
         }
     }
 
-    private String getMessageBy(Rank rank) {
-        if (rank == Rank.SECOND) {
+    private static String getMessageBy(Rank rank) {
+        if (rank.hasBonus()) {
             return RESULT_SHEET_HAS_BONUS_MESSAGE;
         }
         return RESULT_SHEET_DONT_HAVE_MESSAGE;
     }
 
-    public void printRateOfReturn(double rateOfReturn) {
+    public static void printRateOfReturn(double rateOfReturn) {
         System.out.printf(PRINT_RATE_OF_RETURN_MESSAGE, rateOfReturn);
+    }
+
+    public static void printError(String errorMessage) {
+        System.out.println(PREFIX_ERROR_MESSAGE + errorMessage);
     }
 }
