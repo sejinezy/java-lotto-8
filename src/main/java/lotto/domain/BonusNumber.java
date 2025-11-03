bfd283177ec0ee702d2c9c882d72d4510166d758
sejin <psj8880@naver.com>
2025-11-03 15:22:42 +0900

    refactor(BonusNumber): 보너스 번호와 당첨 숫자 사이의 중복 검증 로직 삭제
    - 보너스 번호와 당첨 숫자 사이의 중복 검증은 두 필드를 가진 WinningLotto 클래스에서 수행하도록 함

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,40 +1,14 @@
 package lotto.domain;
 
-import java.util.List;
+import lotto.domain.validator.Validator;
 
 public class BonusNumber {
 
-    private static final String ERR_NUMBER_RANGE = "[ERROR] 1 ~ 45 사이의 숫자만 가능합니다.";
-    private static final String ERR_DUPLICATE_BONUS = "[ERROR] 보너스 번호는 당첨 숫자와 중복될 수 없습니다.";
-
     private final int bonus;
 
-    public BonusNumber(String bonusInput, WinningNumbers winningNumbers) {
-        int bonus = parseInteger(bonusInput);
-        validateRange(bonus);
-        validateUniqueNumber(winningNumbers);
-        this.bonus = bonus;
-    }
-
-    private int parseInteger(String bonusInput) {
-        try {
-            return Integer.parseInt(bonusInput);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 가능합니다.");
-        }
-    }
-
-    private void validateRange(Integer number) {
-        if (number < 1 || number > 45) {
-            throw new IllegalArgumentException(ERR_NUMBER_RANGE);
-        }
-    }
-
-    private void validateUniqueNumber(WinningNumbers winningNumbers) {
-        List<Integer> winning = winningNumbers.getWinningNumbers();
-        if (winning.contains(bonus)) {
-            throw new IllegalArgumentException(ERR_DUPLICATE_BONUS);
-        }
+    public BonusNumber(int bonusInput) {
+        Validator.range(bonusInput);
+        this.bonus = bonusInput;
     }
 
     public int getBonus() {
