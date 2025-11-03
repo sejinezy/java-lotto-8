d99496c56b8e0da620ac0ba0474023f1a15c7bb0
sejin <psj8880@naver.com>
2025-11-03 14:43:11 +0900

    feat(Lotto): 보너스 번호 포함 여부 확인 메서드 구현
    - 해당 로또 번호에 보너스 번호가 포함 되어 있는 지 아닌 지 확인 하는 메서드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,37 +1,17 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
+import lotto.domain.validator.Validator;
 
-public class Lotto {
-    private static final String ERR_LOTTO_SIZE = "[ERROR] 로또 번호는 6개여야 합니다.";
-    private static final String ERR_DUPLICATE_NUMBERS = "[ERROR] 로또 번호는 중복되면 안됩니다.";
+public record Lotto(List<Integer> numbers) {
 
-    private final List<Integer> numbers;
-
-    public Lotto(List<Integer> numbers) {
-        validateSize(numbers);
-        validateDuplicate(numbers);
-        this.numbers = numbers;
+    public Lotto {
+        Validator.size(numbers);
+        Validator.duplication(numbers);
     }
 
-    private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ERR_LOTTO_SIZE);
-        }
-    }
-
-    private void validateDuplicate(List<Integer> numbers) {
-        if (numbers.size() != new HashSet<>(numbers).size()) {
-            throw new IllegalArgumentException(ERR_DUPLICATE_NUMBERS);
-        }
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
+    public boolean contains(BonusNumber bonusNumber) {
+        return numbers.contains(bonusNumber.getBonus());
     }
 
     @Override
