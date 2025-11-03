b1ca02b8edf70a7842842fc4d13efc13dd27c411
sejin <psj8880@naver.com>
2025-11-03 15:26:29 +0900

    refactor(DefaultPickRandomValues): 매직 넘버 상수 처리
    - 비즈니스적으로 의미가 있는 값들이므로 의미가 잘 드러나는 이름으로 상수 처리
    - 가독성도 좋아지고, 추후 값 변경시에도 편리함

--- src/main/java/lotto/infra/random/DefaultPickRandomValues.java
+++ src/main/java/lotto/infra/random/DefaultPickRandomValues.java
@@ -6,8 +6,12 @@ import lotto.domain.port.PickRandomNumbers;
 
 public class DefaultPickRandomValues implements PickRandomNumbers {
 
+    private static final int NUMBER_MIN = 1;
+    private static final int NUMBER_MAX = 45;
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+
     @Override
     public List<Integer> pickRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(NUMBER_MIN, NUMBER_MAX, LOTTO_NUMBERS_SIZE);
     }
 }
