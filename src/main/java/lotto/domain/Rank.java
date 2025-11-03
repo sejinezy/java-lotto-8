8b33967efb7a312b50d84e59024d569ac4ea9445
sejin <psj8880@naver.com>
2025-11-03 22:34:47 +0900

    refactor(Rank): 순서 조정
    - Rank.values()로 출력 순서를 맞추기 위해 Rank 순서 조정
    - hasBonus 메서드 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6, false, 2000000000), SECOND(5, true, 30000000),
-    THIRD(5, false, 1500000), FOURTH(4, false, 50000), FIFTH(3, false, 5000);
+    FIFTH(3, false, 5000),
+    FOURTH(4, false, 50000),
+    THIRD(5, false, 1500000),
+    SECOND(5, true, 30000000),
+    FIRST(6, false, 2000000000);
 
     private final int numberOfMatches;
     private final boolean hasBonus;
@@ -29,5 +32,9 @@ public enum Rank {
 
     public int getPrice() {
         return price;
+    }
+
+    public boolean hasBonus() {
+        return hasBonus;
     }
 }
