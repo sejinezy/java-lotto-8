70b24ea7fccf7ba86bb85fdde2104886e38dc0a0
sejin <psj8880@naver.com>
2025-11-03 22:47:57 +0900

    refactor(Controller): 메서드 쪼개기 및 메서드명 수정
    - 의미가 더 드러나도록 메서드명 리팩터링
    - run() 하위 단계 메서드로 의미 단위 분리 -> 가독성 향상

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,7 +1,5 @@
 package lotto;
 
-import static lotto.view.InputParser.*;
-
 import java.util.List;
 import lotto.domain.BonusNumber;
 import lotto.domain.LottoCart;
@@ -9,6 +7,7 @@ import lotto.domain.PurchaseAmount;
 import lotto.domain.WinningLotto;
 import lotto.domain.WinningNumbers;
 import lotto.domain.ResultSheet;
+import lotto.view.InputParser;
 import lotto.view.InputRetryHandler;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -17,59 +16,74 @@ public class Controller {
 
     private final InputView inputView;
     private final LottoUseCase lottoUseCase;
-    private final OutputView outputView;
 
-    public Controller(InputView inputView, LottoUseCase lottoUseCase, OutputView outputView) {
+
+    public Controller(InputView inputView, LottoUseCase lottoUseCase) {
         this.inputView = inputView;
         this.lottoUseCase = lottoUseCase;
-        this.outputView = outputView;
     }
 
     public void run() {
-        PurchaseAmount purchaseAmount = getPurchaseAmount();
+        PurchaseAmount purchaseAmount = askPurchaseAmount();
 
-        LottoCart lottoCart = lottoUseCase.generateLottoCart(purchaseAmount);
-        outputView.printLottoCount(lottoCart);
-        outputView.printLottoCart(lottoCart);
+        LottoCart lottoCart = issueLottos(purchaseAmount);
+        showIssuedLottos(lottoCart);
 
-        WinningNumbers winningNumbers = getWinningNumbers();
-        BonusNumber bonusNumber = getBonusNumber();
-        WinningLotto winningLotto = validWinningLotto(winningNumbers, bonusNumber);
-
+        WinningLotto winningLotto = askWinningLotto();
         ResultSheet resultSheet = lottoUseCase.compare(winningLotto, lottoCart);
-        double rateOfReturn = lottoUseCase.calculateRateOfReturn(winningLotto, lottoCart, purchaseAmount);
 
-        outputView.printWinningStatistics(resultSheet);
-        outputView.printRateOfReturn(rateOfReturn);
+        showStatistics(resultSheet, purchaseAmount);
     }
 
-    private BonusNumber getBonusNumber() {
-        return InputRetryHandler.askUtilValid(() -> {
-            String bonusInput = validateBlank(inputView.bonusNumberReadLine());
-            return new BonusNumber(changeType(bonusInput));
+    private LottoCart issueLottos(PurchaseAmount purchaseAmount) {
+        return lottoUseCase.generateLottoCart(purchaseAmount);
+    }
+
+    private void showIssuedLottos(LottoCart lottoCart) {
+        OutputView.printLottoCount(lottoCart);
+        OutputView.printLottoCart(lottoCart);
+    }
+
+    private WinningLotto askWinningLotto() {
+        WinningNumbers winningNumbers = getWinningNumbers();
+        BonusNumber bonusNumber = getBonusNumber();
+        return createValidWinningLotto(winningNumbers, bonusNumber);
+    }
+
+    private void showStatistics(ResultSheet resultSheet, PurchaseAmount purchaseAmount) {
+        double rateOfReturn = lottoUseCase.calculateRateOfReturn(resultSheet, purchaseAmount);
+
+        OutputView.printWinningStatistics(resultSheet);
+        OutputView.printRateOfReturn(rateOfReturn);
+    }
+
+    private PurchaseAmount askPurchaseAmount() {
+        return InputRetryHandler.askUntilValid(() -> {
+            String amountInput = InputParser.validateBlank(inputView.purChaseAmountReadLine());
+            return new PurchaseAmount(InputParser.toInt(amountInput));
         });
     }
 
     private WinningNumbers getWinningNumbers() {
-        return InputRetryHandler.askUtilValid(() -> {
-            List<String> winningNumbersInput = parseValidateNumbers(inputView.winningNumbersReadLine());
-            return new WinningNumbers(changeType(winningNumbersInput));
+        return InputRetryHandler.askUntilValid(() -> {
+            List<String> winningNumbersInput = InputParser.parseValidateNumbers(inputView.winningNumbersReadLine());
+            return new WinningNumbers(InputParser.toInts(winningNumbersInput));
         });
     }
 
-    private PurchaseAmount getPurchaseAmount() {
-        return InputRetryHandler.askUtilValid(() -> {
-            String amountInput = validateBlank(inputView.purChaseAmountReadLine());
-            return new PurchaseAmount(changeType(amountInput));
+    private BonusNumber getBonusNumber() {
+        return InputRetryHandler.askUntilValid(() -> {
+            String bonusInput = InputParser.validateBlank(inputView.bonusNumberReadLine());
+            return new BonusNumber(InputParser.toInt(bonusInput));
         });
     }
 
-    private WinningLotto validWinningLotto(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+    private WinningLotto createValidWinningLotto(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
         while (true) {
             try {
                 return new WinningLotto(winningNumbers, bonusNumber);
             } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
+                OutputView.printError(e.getMessage());
                 bonusNumber = getBonusNumber();
             }
         }
